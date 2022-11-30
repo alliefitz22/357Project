fc3c078677bb07c3017e44538b1ed037191dc798
alliefitz22 <95373583+alliefitz22@users.noreply.github.com>
2022-11-30 10:54:20 -0500

    Added skeleton transitions

--- app/src/main/java/com/example/a357project/CorrectAnswerActivity.kt
+++ app/src/main/java/com/example/a357project/CorrectAnswerActivity.kt
@@ -1,11 +1,30 @@
 package com.example.a357project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class CorrectAnswerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_correct_answer)
+        val continueButton = findViewById<Button>(R.id.nextQ)
+        val stopButton = findViewById<Button>(R.id.cHome)
+        val currentStreak = findViewById<TextView>(R.id.cCurrentStreak)
+        val longestStreak = findViewById<TextView>(R.id.cLongestStreak)
+
+        continueButton.setOnClickListener {
+            //This is where we need to go back to the question screen
+            //val i = Intent(this, QuestionActivity::class.java)
+            //startActivity(i)
+        }
+        stopButton.setOnClickListener {
+            //this is where we need to go back to the main screen
+           // val i = Intent(this, MainActivity::class.java )
+           // startActivity(i)
+        }
+
     }
 }
--- app/src/main/java/com/example/a357project/WrongAnswerActivity.kt
+++ app/src/main/java/com/example/a357project/WrongAnswerActivity.kt
@@ -2,10 +2,30 @@ package com.example.a357project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class WrongAnswerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wrong_answer)
+        val tryAgainButton = findViewById<Button>(R.id.tryAgainButton)
+        val stopButton = findViewById<Button>(R.id.wHome)
+        val currentStreak = findViewById<TextView>(R.id.wcurrentStreak)
+        val longestStreak = findViewById<TextView>(R.id.wLongestStreak)
+
+        tryAgainButton.setOnClickListener {
+            //This is where we need to go back to the question screen
+            //val i = Intent(this, QuestionActivity::class.java)
+            //startActivity(i)
+            //Also add code to reset streak
+        }
+        stopButton.setOnClickListener {
+            //Add code to reset streak
+            //this is where we need to go back to the main screen
+            // val i = Intent(this, MainActivity::class.java )
+            // startActivity(i)
+
+        }
     }
 }
