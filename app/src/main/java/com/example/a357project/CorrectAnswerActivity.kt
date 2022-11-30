130680b8e981c0f3e024ae73f3a445e388654982
alliefitz22 <95373583+alliefitz22@users.noreply.github.com>
2022-11-29 22:24:24 -0500

    Added the wrong answer activity and the correct answer activity. Absolutely no functionality.

---
+++ app/src/main/java/com/example/a357project/CorrectAnswerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a357project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CorrectAnswerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_correct_answer)
+    }
+}
