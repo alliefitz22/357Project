47fc0637686498db641829c179109e093fdbc20d
Trevor Martin <marttrev@mail.gvsu.edu>
2022-11-29 22:48:56 -0500

    Added logic to initialize used question file in local storage.

--- app/src/main/java/com/example/a357project/QuestionActivity.kt
+++ app/src/main/java/com/example/a357project/QuestionActivity.kt
@@ -1,11 +1,26 @@
 package com.example.a357project
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.BufferedReader
 
 class QuestionActivity : AppCompatActivity() {
+
+    lateinit var used: Array<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
+
+        val files: Array<String> = applicationContext.fileList()
+        if (!files.contains("used")) {
+            applicationContext.openFileOutput("used", Context.MODE_PRIVATE).use {
+                it.write("".toByteArray())
+            }
+        } else {
+            val reader: BufferedReader = applicationContext.openFileInput("used").bufferedReader()
+            used = reader.readLines().toTypedArray()
+        }
     }
 }
